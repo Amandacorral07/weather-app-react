965d883a5d702bcfc230c838d626618fba30982e
Amandacorral07 <amandalewis0707@gmail.com>
2022-05-20 10:36:54 -0700

    Integrated OpenWeatherAPI for daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import { axios } from "axios";
 
-export default function WeatherForecast(){
+export default function WeatherForecast(props){
+  function handleResponse(response){
+    console.log(response.data);
+  }
+  console.log(props);
+  let apiKey="04516b03d862a33f9817076057aa38aa";
+  let longitude= props.coordinates.lon;
+  let latitude= props.coordinates.lat;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  
   return (
     <div className="WeatherForecast">
       <div className="row">
